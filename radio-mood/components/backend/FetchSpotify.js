80f6c4d06e2fd2c93b8f9348ff5f1070f5037b6f
patryk-siewiera-elp <94541618+amber-zeus@users.noreply.github.com>
2021-11-18 17:36:35 +0100

    [feat] front / backend structure

---
+++ radio-mood/components/backend/FetchSpotify.js
@@ -0,0 +1,7 @@
+export const FetchSpotify = () => {
+  return (
+    <div>
+      this is fetching from api
+    </div>
+  )
+}
--- radio-mood/pages/index.js
+++ radio-mood/pages/index.js
@@ -1,5 +1,5 @@
 import Head from 'next/head'
-import {Title, Example, Info} from "../components/index"
+import {Title, Example, Info} from "../components/frontend"
 
 export default function Home() {
   return (
