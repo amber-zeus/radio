9f11c4429b8d889f38d8753b2d113516d0cff917
patryk-siewiera-elp <91964476+patryk-siewiera-elp@users.noreply.github.com>
2021-11-19 21:00:37 +0100

    [feat] paddings between pages

--- radio-mood/pages/albums/[id].js
+++ radio-mood/pages/albums/[id].js
@@ -1,8 +1,18 @@
+import Link from 'next/link'
+
 const Id = () => {
   return (
-    <div>
-      <div>go back</div>
-      this is details page now
+    <div className="mt-4">
+
+      <div className="max-w-md m-auto p-8 border-4 border-black" style={{fontFamily: "MontserratV18Latin900"}}>
+        <Link href="/">
+          go back</Link>
+
+        <br/>
+        <div className="p-8">
+          this is details page now
+        </div>
+      </div>
     </div>
   )
 }
