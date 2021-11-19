e19e15d7e65a227d002cfcbec68d52a53608db9f
patryk-siewiera-elp <91964476+patryk-siewiera-elp@users.noreply.github.com>
2021-11-19 20:18:25 +0100

    [feat] fonts

--- radio-mood/components/frontend/Info.js
+++ radio-mood/components/frontend/Info.js
@@ -2,11 +2,11 @@ export const Info = () => {
   return (
     <div className="italic" style={{fontFamily: "HelveticaNowTextBold"}}>
       <span className="font-extrabold">Album covers and track titles</span>
-      <br/>doesn't tell us much about the mood of the album.
+      <br/>doesn't tell us much about the mood of the album
 
       <br/>
       <br/>
-      But we could show you how vibes changes through the album
+      But we could show you <br/> how vibes changes through the album
       <br/>
     </div>
   )
