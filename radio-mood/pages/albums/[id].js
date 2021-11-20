d14514301d0c799677e921a82705f69a970a8a69
patryk-siewiera-elp <91964476+patryk-siewiera-elp@users.noreply.github.com>
2021-11-20 01:26:46 +0100

    [feat] title and name of album, on details page

--- radio-mood/pages/albums/[id].js
+++ radio-mood/pages/albums/[id].js
@@ -1,6 +1,6 @@
 import Link from 'next/link'
 import {Graph, OneTrackGraph} from "../../components/frontend";
-import { useRouter } from 'next/router'
+import {useRouter} from 'next/router'
 
 
 const AlbumDetails = ({params}) => {
@@ -8,7 +8,7 @@ const AlbumDetails = ({params}) => {
 
   const router = useRouter()
   // debugger
-  if(router === undefined){
+  if (router === undefined) {
     router.push('/')
   }
 
@@ -21,19 +21,25 @@ const AlbumDetails = ({params}) => {
   return (
     <div className="mt-4 mx-2 pb-12">
       <div className="max-w-4xl m-auto p-8 border-2 border-black" style={{fontFamily: "MontserratV18Latin900"}}>
-        <Link href="/" className="">
-          <div className="text-3xl border border-2 w-52 text-center p-2 hover:border-blue-900 cursor-pointer text-blue-700">
-            go back
-          </div>
-        </Link>
-        <br/>
-
-
-        {artist} - {album}
-
-        <div>
-          this is details page now
+        <div className="mb-10">
+          <Link href="/" className="">
+            <div
+              className="text-2xl border border-2 w-40 border-gray-300 text-center p-2 hover:border-black cursor-pointer text-blue-900" style={{fontFamily: "HelveticaNowDisplayBlk"}}>
+              go back
+            </div>
+          </Link>
         </div>
+
+
+        <div className="bg-blue-700 text-white p-10 text-center" style={{fontFamily: "MontserratV18Latin900"}}>
+          <div className="text-5xl ">
+            {artist}
+          </div>
+          <div className="text-3xl mt-2">
+            {album}
+          </div>
+        </div>
+
 
         <div className="m-auto p-10 text-center flex align-center">
           <Graph/>
@@ -42,8 +48,8 @@ const AlbumDetails = ({params}) => {
         <div className="text-center m-auto flex align-center">
           <OneTrackGraph/>
         </div>
-
       </div>
+
     </div>
   )
 }
